a172cd5f7a3bd83d4aa29f6bedc1db47d9eb2a38
Erick Romero <erickromeronieto018@gmail.com>
2024-03-22 12:52:11 -0500

    configured the routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersHomeComponent } from './users/pages/users-home/users-home.component';
+import { UserInformationComponent } from './users/pages/user-information/user-information.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: UsersHomeComponent,
+  },
+  {
+    path: 'users-information',
+    component: UserInformationComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
